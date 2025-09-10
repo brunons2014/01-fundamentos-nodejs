b731d1f2a1042bab0b94329e3936f49e4bb6ce99
Bruno Henrique <62818096+brunons2014@users.noreply.github.com>
2025-09-10 00:01:12 -0300

    Aprendendo um pouco sobre GET e POST

--- src/server.js
+++ src/server.js
@@ -9,14 +9,29 @@ import http  from 'node:http'
 // GET /users => Listar usuários
 // POST /users => Cadastrar um usuário
 
- const server = http.createServer((req, res) => {
+// Stateful - Stateless
+// Stateful => O servidor guarda alguma informação do cliente (Sessão, Cookies)
+// Stateless => A cada requisição o cliente precisa mandar todas as informações para o servidor (Autenticação, Token, JWT)
+
+// Cabeçalhos (Requisição e Resposta) => Metadados (Informações sobre a requisição ou resposta)
+
+const users = []
+ 
+const server = http.createServer((req, res) => {
     const { method, url } = req
 
     if (method === 'GET' && url === '/users') {
-        return res.end('Listagem de usuários')
+        return res
+        .setHeader('Content-Type', 'application/json')
+        .end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@example.com'
+        })
         return res.end('Cadastro de usuários')
     }
 
